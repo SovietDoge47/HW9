64459fbaae8b6441107ea9c3327474faa5bb8001
Zac Miller <zmiller403@gmail.com>
2021-03-03 18:11:11 -0600

    changed stuff

---
+++ LinkedList.cpp
@@ -0,0 +1,215 @@
+#include "LinkedList.hpp"
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+LinkedList::LinkedList()
+{
+    this->head = NULL;
+    this->tail = NULL;
+    this->count = 0;
+}
+
+void LinkedList::addAtIndex(int index, int value)
+{
+    if(index < 0 || index > this->count)
+    {
+        cout << "ArrayIndexOutOfBoundException!!!!" << endl;
+    }
+    else
+    {
+        if(index == 0)
+        {
+            this->addStart(value);
+        }
+        else if(index == this->count)
+        {
+            this->addEnd(value);
+        }
+        else
+        {
+            Node* precedingNode = this->head;
+            Node* followingNode = this->head;
+            for(int i = 0; i < index-1; i++)
+            {
+                precedingNode = precedingNode->getNextNode();
+                followingNode = followingNode->getNextNode();
+            }
+            followingNode = followingNode->getNextNode();
+            Node* newNode = new Node(value);
+            precedingNode->setNextNode(newNode);
+            newNode->setNextNode(followingNode);
+            this->count++;
+        }
+    }
+}
+
+int LinkedList::removeAtIndex(int index)
+{
+    if(this->head)
+    {
+        if(index < 0 || index >= this->count)
+        {
+            cout << "ArrayIndexOutOfBoundException!!!!" << endl;
+        }
+        else
+        {
+            //I actuall have something to remove
+            if(index == 0)
+            {
+                return this->removeStart();
+            }
+            else if(index == this->count-1)
+            {
+                return this->removeEnd();
+            }
+            else
+            {
+                //we are removing from somewhere in the middle
+                Node* prevDude = this->head;
+                Node* dude2Remove = this->head;
+
+                //How do I get prevDude and dude2Remove set in a single loop?
+                //run dude2Remove to the correct index
+                for(int i = 0; i < index; i++)
+                {
+                    dude2Remove = dude2Remove->getNextNode();
+                }
+
+                //now make preDude point to the node right before dude2Remove
+                while(prevDude->getNextNode() != dude2Remove)
+                {
+                    prevDude = prevDude->getNextNode();
+                }
+
+                /* Identical result as the while loop above
+                //what is another way we could have written the while loop above?
+                for(int i = 0; i < index-1; i++)
+                {
+                    prevDude = prevDude->getNextNode();
+                }
+                */
+
+               //Now everything is position and we are ready to operate!!!
+               prevDude->setNextNode(dude2Remove->getNextNode());
+               dude2Remove->setNextNode(NULL);
+               int value2Return = dude2Remove->getPayload();
+               delete(dude2Remove);
+               this->count--;
+               return value2Return;
+            }
+        }
+    }
+    else
+    {
+        cout << "Nothing to Remove from the Empty List" << endl;
+    }    
+}
+
+void LinkedList::addEnd(int value)
+{
+    Node* n = new Node(value);
+    if(head == NULL)
+    {
+        //we have the empty list
+        this->head = n;
+        this->tail = n;
+    }
+    else
+    {
+        //we have a list with at least one element in it, and we know that tail
+        //points to the end of that list.
+        this->tail->setNextNode(n);
+        this->tail = n;
+    }
+    this->count++;
+}
+
+void LinkedList::addStart(int value)
+{
+    Node* n = new Node(value);
+    if(this->head == NULL)
+    {
+        //we have the empty list
+        this->head = n;
+        this->tail = n;
+    }
+    else
+    {
+        //we have a list with at least one element in it, and we know that head
+        //points to the start of that list.
+        n->setNextNode(this->head);
+        this->head = n;
+    }
+    this->count++;
+}
+
+int LinkedList::removeEnd()
+{
+    if(this->count > 0)
+    {
+        Node* n = this->tail;
+        int value = n->getPayload();
+        if(this->count == 1)
+        {
+            this->head = NULL;
+            this->tail = NULL;
+            
+        }
+        else
+        {
+            Node* currNode = this->head;
+            while(currNode->getNextNode() != this->tail)
+            {
+                currNode = currNode->getNextNode();
+            }
+            this->tail = currNode;
+            this->tail->setNextNode(NULL);
+        }
+        this->count--;
+        delete(n); //if we didn't do this, we technically have a memory leak
+        return value;
+    }
+}
+
+int LinkedList::removeStart()
+{
+    if(this->count > 0)
+    {
+        Node* n = this->head;
+        int value = n->getPayload();
+        if(this->count == 1)
+        {
+            this->head = NULL;
+            this->tail = NULL;
+        }
+        else
+        {
+            Node* currNode = this->head;
+            currNode = currNode->getNextNode();
+            this->head = currNode;
+        }
+        this->count--;
+        delete(n); //if we didn't do this, we technically have a memory leak
+        return value;
+    }
+}
+
+void LinkedList::display()
+{
+    if(this->count == 0)
+    {
+        cout << "the empty list" << endl;
+    }
+    else
+    {
+        Node* currNode = this->head;
+        while(currNode->getNextNode() != NULL)
+        {
+            cout << currNode->getPayload() << " -> ";
+            currNode = currNode->getNextNode();
+        }
+        cout << currNode->getPayload() << endl;
+    }
+}
---
+++ LinkedList.hpp
@@ -0,0 +1,19 @@
+#include "Node.hpp"
+
+class LinkedList
+{
+    private:
+        Node* head;
+        Node* tail;
+        int count;
+
+    public:
+        LinkedList();
+        void display();
+        void addEnd(int value);
+        int removeEnd();
+        void addStart(int value);
+        int removeStart();
+        int removeAtIndex(int index);
+        void addAtIndex(int index, int value);
+};
